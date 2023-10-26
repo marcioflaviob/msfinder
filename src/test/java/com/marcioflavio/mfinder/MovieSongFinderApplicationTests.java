d99b8fcce03d49b645826858ecd5fd1b9975b7fe
Marcio Flavio <oi@marcioflavio.com>
2023-10-25 21:07:03 -0300

    First commit, nothing done

---
+++ src/test/java/com/marcioflavio/mfinder/MovieSongFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcioflavio.mfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieSongFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
