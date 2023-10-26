48bf8838247c0613224832591141fb5fac37d44d
Marcio Flavio <oi@marcioflavio.com>
2023-10-26 12:07:41 -0300

    Song search working

--- src/main/java/com/marcioflavio/mfinder/MovieSongFinderApplication.java
+++ src/main/java/com/marcioflavio/mfinder/MovieSongFinderApplication.java
@@ -1,13 +1,22 @@
 package com.marcioflavio.mfinder;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import core.GLA;
 
 @SpringBootApplication
 public class MovieSongFinderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieSongFinderApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		//SpringApplication.run(MovieSongFinderApplication.class, args);
+        GLA gla = new GLA();
+        System.out.println("Searching...");
+        long startMs = System.currentTimeMillis();
+        System.out.println(gla.search("lucid dreams").getHits().get(0).fetchLyrics());
+        System.out.println(System.currentTimeMillis() - startMs + "ms");
 	}
 
 }
---
+++ src/main/java/com/marcioflavio/mfinder/config/GeniusAPIConfig.java
@@ -0,0 +1,18 @@
+package com.marcioflavio.mfinder.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class GeniusAPIConfig {
+    
+    @Value("${genius.api.key}") //Grab the value from application.properties instead of storing it in the code.
+    private String apiKey;
+
+    private String getApiKey() {
+        return apiKey;
+    }
+
+}
